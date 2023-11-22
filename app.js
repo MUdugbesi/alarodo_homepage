5bae081c508890f54a127ad74b5982b6eb071581
Marvelous <marvelous.udugbesi@icloud.com>
2023-11-22 18:56:35 +0000

    second commit, updated code

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-const theme_button = document.querySelector('#theme_btn');
+const theme_button = document.querySelector('.theme_btn');
 const moon_fill = document.querySelector('.moon_fill');
 const sun_fill = document.querySelector('.sun_fill');
 const logo = document.querySelector('#logo');
 const body = document.querySelector('body')
 const container = document.querySelector('.container');
 const about = document.querySelector('a');
-const smallText = document.querySelector('.small_text') 
-const largeText = document.querySelector('#large_text') 
+const smallText = document.querySelector('.small_text');
+const largeText = document.querySelector('#large_text');
 
-function setLightTheme() {
-    const moon_fill = document.querySelector('.moon_fill');
-    const sun_fill = document.querySelector('.sun_fill');
-    sun_fill.style.border = ''
-    sun_fill.style.backgroundColor = ''
-    moon_fill.style.border = '';
-    moon_fill.style.borderRadius = ''
-    moon_fill.style.backgroundColor = ''
+
+
+
+
+
+function setLightTheme(e1, e2, e3, e4) {
+    e1.style.border = ''
+    e1.style.backgroundColor = ''
+    e2.style.border = '';
+    e2.style.borderRadius = ''
+    e2.style.backgroundColor = ''
     logo.src = 'assets/alarado-icon-homepage.svg';
-    body.style.backgroundColor = ''
-    container.style.backgroundColor = ''
+    e3.style.backgroundColor = ''
+    e4.style.backgroundColor = ''
     about.style.color = ''
-    sun_fill.src = 'assets/Sun_fill.svg';
+    e1.src = 'assets/Sun_fill.svg';
     smallText.style.color = '';
     largeText.style.color = ''
 
-    theme_button.addEventListener('click', () => setDarkTheme(sun_fill, moon_fill, body, container))
+    theme_button.addEventListener('click', () => setDarkTheme(sun_fill, moon_fill, body, container));
+    mobile_theme_button.addEventListener('click', () => setDarkTheme(mobile_sun_fill, mobile_moon_fill, body, container))
 }
+
+
 
 function setDarkTheme(e1, e2, e3, e4) {
     e1.style.border = 'none'
@@ -39,11 +45,44 @@ function setDarkTheme(e1, e2, e3, e4) {
     about.style.color = 'white';
     smallText.style.color = '#fff';
     largeText.style.color = '#fff'
-    
 
-    theme_button.addEventListener('click', () => setLightTheme())
+
+    theme_button.addEventListener('click', () => setLightTheme(sun_fill, moon_fill, body, container))
+    mobile_theme_button.addEventListener('click', () => setLightTheme(mobile_sun_fill, mobile_moon_fill, body, container))
 }
 
 
-theme_button.addEventListener('click', () => setDarkTheme(sun_fill, moon_fill, body, container))
+theme_button.addEventListener('click', () => setDarkTheme(sun_fill, moon_fill, body, container, theme_button));
+
+const barsSolid = document.querySelector('.bars-solid');
+const mobileNav = document.querySelector('.mobile_navigation');
+const cancelSolid = document.querySelector('#cancel-solid');
+
+
+function openNavBar() {
+    mobileNav.style.display = 'block';
+
+}
+const closeNavBar = () => {
+    mobileNav.style.display = 'none';
+
+}
+
+barsSolid.addEventListener('click', openNavBar);
+cancelSolid.addEventListener('click', closeNavBar);
+
+
+// Mobile theme changer
+
+const mobile_theme_button = document.querySelector('.mobile_theme_btn');
+const mobile_moon_fill = document.querySelector('.mobile_moon_fill');
+const mobile_sun_fill = document.querySelector('.mobile_sun_fill');
+
+
+mobile_theme_button.addEventListener('click', () => {
+    setDarkTheme(mobile_sun_fill, mobile_moon_fill, body, container, mobile_theme_button);  
+});
+
+
+module.exports = { setDarkTheme, setLightTheme }
 
